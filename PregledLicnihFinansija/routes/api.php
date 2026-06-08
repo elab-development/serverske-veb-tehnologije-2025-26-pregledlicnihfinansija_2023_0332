2f62254922e410cc8157122540bd18aa23ad90e0
20230571ts <ts20230571@gmail.com>
2026-06-08 18:21:54 +0200

    Promene u kontroleru Kategorija

--- PregledLicnihFinansija/routes/api.php
+++ PregledLicnihFinansija/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KategorijaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::apiResource('kategorije', KategorijaController::class)->except(['index', 'show']);
 });
