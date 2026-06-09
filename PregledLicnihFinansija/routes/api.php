ebd3f8998a04389c808adc1845fba43a308560ec
20230571ts <ts20230571@gmail.com>
2026-06-09 09:54:04 +0200

    Dodat IsAdmin middleware za zastitu kategorija

--- PregledLicnihFinansija/routes/api.php
+++ PregledLicnihFinansija/routes/api.php
@@ -4,17 +4,23 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KategorijaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Middleware\IsAdmin;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/kategorije', [KategorijaController::class, 'index']);
-Route::get('/kategorije/{id}', [KategorijaController::class, 'show']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::apiResource('kategorije', KategorijaController::class)->except(['index', 'show']);
+
+    Route::get('/kategorije', [KategorijaController::class, 'index']);
+    Route::get('/kategorije/{id}', [KategorijaController::class, 'show']);
+    
+    Route::middleware(IsAdmin::class)->group(function () {
+        Route::apiResource('kategorije', KategorijaController::class)->except(['index', 'show']);
+    });
 });
