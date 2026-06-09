0d8852ce2a35f203f389afe004904e415286a07d
20230571ts <ts20230571@gmail.com>
2026-06-09 11:26:47 +0200

    Dodati su LimitController i rute za limite

--- PregledLicnihFinansija/routes/api.php
+++ PregledLicnihFinansija/routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KategorijaController;
 use App\Http\Controllers\TransakcijaController;
+use App\Http\Controllers\LimitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Middleware\IsAdmin;
+
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -32,4 +34,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/transakcije', [TransakcijaController::class, 'store']);
     Route::get('/transakcije/filter', [TransakcijaController::class, 'filter']);
     Route::delete('/transakcije/{id}', [TransakcijaController::class, 'destroy']);
+
+    Route::apiResource('limiti', LimitController::class)->except(['create', 'edit']);
 });
