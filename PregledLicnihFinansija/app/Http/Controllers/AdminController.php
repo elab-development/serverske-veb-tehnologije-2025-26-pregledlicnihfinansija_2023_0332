d37a3e3fde38407a99f2872e8611605aaee93679
20230571ts <ts20230571@gmail.com>
2026-06-13 18:06:43 +0200

    Dodat AdminController koji ima f-je za vracanje liste korisnika, promenu uloga i analitiku

---
+++ PregledLicnihFinansija/app/Http/Controllers/AdminController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Transakcija;
+use App\Models\Kategorija;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function korisnici()
+    {
+        $korisnici = User::all();
+        return response()->json($korisnici);
+    }
+
+    public function promeniUlogu(Request $request, $id)
+    {
+        $request->validate([
+            'type' => 'required|in:klijent,administrator',
+        ]);
+
+        $korisnik = User::findOrFail($id);
+        $korisnik->update(['type' => $request->type]);
+
+        return response()->json(['poruka' => 'Uloga je uspesno promenjena!', 'korisnik' => $korisnik]);
+    }
+
+    public function analitika()
+    {
+        $ukupnoKorisnika = User::count();
+        $ukupnoTransakcija = Transakcija::count();
+        $ukupnoKategorija = Kategorija::count();
+        $transakcijePoKategoriji = Transakcija::with('kategorija') 
+            ->selectRaw('kategorija_id, count(*) as broj, sum(kolicina) as ukupno')
+            ->groupBy('kategorija_id')
+            ->get(); 
+
+        $transakcijePoMesecu = Transakcija::selectRaw('YEAR(datum) as godina, MONTH(datum) as mesec, count(*) as broj, sum(kolicina) as ukupno')
+            ->groupBy('godina', 'mesec')
+            ->orderBy('godina', 'desc')
+            ->orderBy('mesec', 'desc')
+            ->get();
+            
+          return response()->json([
+            'ukupno_korisnika' => $ukupnoKorisnika,
+            'ukupno_transakcija' => $ukupnoTransakcija,
+            'ukupno_kategorija' => $ukupnoKategorija,
+            'transakcije_po_kategoriji' => $transakcijePoKategoriji,
+            'transakcije_po_mesecu' => $transakcijePoMesecu,
+        ]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
