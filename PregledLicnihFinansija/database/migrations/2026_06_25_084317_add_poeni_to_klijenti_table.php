928444686e4193bbb629139bc8cd856e52f6e04e
vmandic0 <v.mandic04@gmail.com>
2026-06-25 17:23:02 +0200

    Dodat zaboravljen $ u migraciji

--- PregledLicnihFinansija/database/migrations/2026_06_25_084317_add_poeni_to_klijenti_table.php
+++ PregledLicnihFinansija/database/migrations/2026_06_25_084317_add_poeni_to_klijenti_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('klijenti', function (Blueprint $table) {
-            table->dropColumn(['poeni', 'bedz']);
+            $table->dropColumn(['poeni', 'bedz']);
         });
     }
 };
